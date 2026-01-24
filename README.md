2eb4dc9876b14ecf04f71a02ff8514b17a2f9c5a
JoCsadi <153603873+joelcsadi@users.noreply.github.com>
2026-01-24 14:33:50 +0000

    Visualizing data/creating notebook

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A folder of machine learning algorithms written by scratch as an interactive exercise to understand their respective mechanics and processes.
+They inlcude:
+    -Simple Linear regression with gradient descent optimization
--- SLR.py
+++ SLR.py
@@ -23,7 +23,18 @@ where n is the number of observations and yi is ith actual response variable
 
 
 We can find the parameters that lead to the global minimum cost function.
+
+Method:
+    1. Initialise the beta_0 and beta_1 to a value of 0.
+    2. Calculate the Cost function MSE(Mean squared error) = MSE = (Sum(yi-y_hat)^2) / n
+        where y_hat at i is Beta_0 + Beta1 * xi. This is done to measure the cost of the model
+        parameters.
+    3. Calculate the derivative/ gradient of the Cost functions in respect to each parameter.
+       This is done using the chain rule. We keep changing the parameters until the reach the 
+
 """
+
+
 x = [1,4,5,7,9,14,20]
 y = [1,5,12,16,20,24,40]
 
@@ -44,8 +55,6 @@ else:
     MSE = MSE / len(y_hat)
 print(MSE)
 
-
-    
 
 
 
