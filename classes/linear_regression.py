423e93e98aa5d6976867287ae7b461b9244ee092
JoCsadi <153603873+joelcsadi@users.noreply.github.com>
2026-01-26 17:08:36 +0000

    Completed Multiple Linear Regression

--- classes/linear_regression.py
+++ classes/linear_regression.py
@@ -14,31 +14,32 @@ where:
 """
 
 class LinearRegression:
-    def __init__(self, learning_rate = 0.005, epochs= 1000):
+    def __init__(self, learning_rate = 0.001, epochs= 1000):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.Beta = None
-    
 
     """
     fit(X,y) finds the parameters that will fit the best line.
     It assumes that even though there is one feature, it should be a np matrix and not
     a 1D vector. The same applies for the Y vector, which should also be a np matrix.
 
-
     """
     def fit(self,X,Y):
+        # Ensure a list or dataframe will turn to a np array
         X = np.array(X)
         Y = np.array(Y)
-        n_observations = X.shape[0]
+        # If the features vector has only one feature, turn it into a Matrix with Rx1 dimensions
         if X.ndim == 1:
             X=X.reshape(-1,1)
-
-        X_design = np.hstack((np.ones((n_observations,1)),X))
-        self.Beta = np.zeros((X_design.shape[1],1))
         Y = Y.reshape(-1,1)
+
+        n_observations = X.shape[0]
+        # Design matrix with a column of 1s, and each other column is the respective feature column
+        X_design = np.hstack((np.ones((n_observations,1)),X))
+        # A parameter column vector (np matrix) that is initialised to 0 each parameter
+        self.Beta = np.zeros((X_design.shape[1],1))
         X_T = X_design.T
-        
         # Now we find the best beta parameters
         for epoch in range(self.epochs):
             Y_hat = X_design @ self.Beta
@@ -49,12 +50,9 @@ class LinearRegression:
         
     def predict(self, X):
         X = np.array(X)
-
         if X.ndim == 1:
             X = X.reshape(-1, 1)
-
         n_observations = X.shape[0]
         X_design = np.hstack((np.ones((n_observations, 1)), X))
-
         return X_design @ self.Beta
 
