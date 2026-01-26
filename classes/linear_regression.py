b080bc5db0726de9222cd6a9066bd467d5beb16f
JoCsadi <153603873+joelcsadi@users.noreply.github.com>
2026-01-26 16:43:54 +0000

    fix

--- classes/linear_regression.py
+++ classes/linear_regression.py
@@ -14,7 +14,7 @@ where:
 """
 
 class LinearRegression:
-    def __init__(self, learning_rate = 0.01, epochs= 1000):
+    def __init__(self, learning_rate = 0.005, epochs= 1000):
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.Beta = None
